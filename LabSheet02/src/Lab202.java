18f71a76a27c5e6e43d0e9c9210c5b336e610119
Goosode <yutdesu@gmail.com>
2024-12-02 13:17:52 +0700

    LabSheet201Example

---
+++ LabSheet02/src/Lab202.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Lab202 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);		
+		
+		while(true) {	
+			//Input 
+			System.out.print("Input number: ");
+			int number = scanner.nextInt();
+			
+			//Min value 
+			int tempNum = Integer.MIN_VALUE;
+			
+			//if tempNum more than number program will stop
+			if(tempNum > number) {
+				System.out.print("BYE BYE");
+				break;
+			}
+			
+			tempNum = number;
+		}
+
+	}
+
+}

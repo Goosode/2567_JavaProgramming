e7d96d35937588038634132d28e01dd4e68ff64b
Goosode <yutdesu@gmail.com>
2025-02-24 11:43:11 +0700

    Homework[24/02/2025]

--- LabSheet10/src/Lab3/Coffee.java
+++ LabSheet10/src/Lab3/Coffee.java
@@ -1,8 +1,6 @@
-package Lab3;
+//package Lab3;
 //==========================================Lab3==========================================
 public class Coffee extends Drink{
-	//FIX
-	
 	//Attribute
 	private int typeCoffee;
 	private char sizeCoffee;
@@ -10,7 +8,8 @@ public class Coffee extends Drink{
 	
 	//Constructor
 	public Coffee(int type, char sizeCoffee, int typeCoffee, Barista barista) {
-		super(type, sizeCoffee);
+		super(type, '\u0000');
+		this.sizeCoffee = sizeCoffee;
 		this.typeCoffee = typeCoffee;
 		this.barista = barista;
 	}
@@ -26,13 +25,13 @@ public class Coffee extends Drink{
 	}//getBarista end
 	
 	public String getTypeName() {
-		if(typeCoffee == 1) {
+		if(this.typeCoffee == 1) {
 			return "Americano";
 		}
-		else if(typeCoffee == 2){
+		else if(this.typeCoffee == 2){
 			return "Espresso";
 		}
-		else if(typeCoffee == 3){
+		else if(this.typeCoffee == 3){
 			return "Cappuccino";
 		}
 		
@@ -40,13 +39,13 @@ public class Coffee extends Drink{
 	}//getTypeName end
 	
 	public int getTypePirce() {
-		if(typeCoffee == 1) {
+		if(this.typeCoffee == 1) {
 			return 50;
 		}
-		else if(typeCoffee == 2){
+		else if(this.typeCoffee == 2){
 			return 55;
 		}
-		else if(typeCoffee == 3){
+		else if(this.typeCoffee == 3){
 			return 65;
 		}
 		
@@ -54,16 +53,16 @@ public class Coffee extends Drink{
 	}//getTypePirce end
 	
 	public String getSizeName() {
-		if(Character.toLowerCase(sizeCoffee) == 's') {
+		if(Character.toLowerCase(this.sizeCoffee) == 's') {
 			return "Short";
 		}
-		else if(Character.toLowerCase(sizeCoffee) == 't') {
+		else if(Character.toLowerCase(this.sizeCoffee) == 't') {
 			return "Tall";
 		}
-		else if(Character.toLowerCase(sizeCoffee) == 'g') {
+		else if(Character.toLowerCase(this.sizeCoffee) == 'g') {
 			return "Grande";
 		}
-		else if(Character.toLowerCase(sizeCoffee) == 'v') {
+		else if(Character.toLowerCase(this.sizeCoffee) == 'v') {
 			return "Venti";
 		}
 		
@@ -71,16 +70,16 @@ public class Coffee extends Drink{
 	}//getSizeName end
 	
 	public int getSizePrice() {
-		if(Character.toLowerCase(sizeCoffee) == 's') {
+		if(Character.toLowerCase(this.sizeCoffee) == 's') {
 			return 100;
 		}
-		else if(Character.toLowerCase(sizeCoffee) == 't') {
+		else if(Character.toLowerCase(this.sizeCoffee) == 't') {
 			return 150;
 		}
-		else if(Character.toLowerCase(sizeCoffee) == 'g') {
+		else if(Character.toLowerCase(this.sizeCoffee) == 'g') {
 			return 200;
 		}
-		else if(Character.toLowerCase(sizeCoffee) == 'v') {
+		else if(Character.toLowerCase(this.sizeCoffee) == 'v') {
 			return 250;
 		}	
 		
@@ -88,6 +87,10 @@ public class Coffee extends Drink{
 	}//getSizePrice end
 	
 	public int getTotalPrice() {
-		return getTypePirce() + getSizePrice();
+		return super.getTypePirce() + this.getTypePirce() + this.getSizePrice();
 	}//getTotalPrice end
+
+	public String toString() {
+		return super.getTypeName() + " " + this.getTypeName() + " (" + this.getSizeName() + ") is " + this.getTotalPrice() + " baht.";
+	}
 }
--- LabSheet10/src/Lab3/CoffeeShopDemo.java
+++ LabSheet10/src/Lab3/CoffeeShopDemo.java
@@ -1,12 +1,56 @@
-package Lab3;
+//package Lab3;
+//==========================================Lab3==========================================
 import javax.swing.*;
 
 public class CoffeeShopDemo {
-	//NOT FINISH
-
 	public static void main(String[] args) {
 		Barista staff1 = new Barista("Leon", 'M');
 		Barista staff2 = new Barista("Claire", 'F');
+
+		//Drink type
+		int typeDrink = Integer.parseInt(JOptionPane.showInputDialog("Drink type: "));
+
+		while (typeDrink != 1 && typeDrink != 2) {
+			typeDrink = Integer.parseInt(JOptionPane.showInputDialog("Drink type, again: "));
+		}
+		
+		//Coffee type
+		int typeCoffee = Integer.parseInt(JOptionPane.showInputDialog("Coffee type: "));
+
+		while (typeCoffee < 1 || typeCoffee > 3) {
+			typeCoffee = Integer.parseInt(JOptionPane.showInputDialog("Coffee type, again: "));
+		}
+
+		//Coffee size
+		String size = JOptionPane.showInputDialog("Coffee size: ");
+		char sizeCoffee = size.charAt(0);
+
+		while (Character.toLowerCase(sizeCoffee) != 's' && Character.toLowerCase(sizeCoffee) != 't' && 
+				Character.toLowerCase(sizeCoffee) != 'g' && Character.toLowerCase(sizeCoffee) != 'v') {
+			size = JOptionPane.showInputDialog("Coffee size, again: ");
+			sizeCoffee = size.charAt(0);
+		}
+
+		//Barista number
+		int barisNum = Integer.parseInt(JOptionPane.showInputDialog("Barista number: "));
+
+		Barista serveStaff;
+		switch (barisNum) {
+			case 2:
+				serveStaff = staff2;
+				break;
+				
+			case 1:
+			default:
+				serveStaff = staff1;
+				break;
+		}
+
+		//OBJ
+		Coffee order = new Coffee(typeDrink, sizeCoffee, typeCoffee, serveStaff);
+		JOptionPane.showMessageDialog(null, order + 
+							"\nBarista: " + order.getBarista().getName() + " (" + order.getBarista().getGenderName() + ")"); 
+		
 	}
 
 }
